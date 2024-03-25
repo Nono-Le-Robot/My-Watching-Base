549f1156b23a72e2ccca93930df56342a81af2d5
NonoLeRobot <sannier.renaud@gmail.com>
2024-03-25 15:51:37 +0100

    wait for genre

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -405,7 +405,7 @@ export default function Home({ groupedBySerie, groupedByMovies }: HomeProps) {
                 />
                 {/* <label htmlFor="pet-select">:</label> */}
 
-                <select
+                {/* <select
                   onChange={(event) => {
                     let selectedGender = "";
                     selectedGender = event.target.value;
@@ -442,7 +442,7 @@ export default function Home({ groupedBySerie, groupedByMovies }: HomeProps) {
                   <option value="Thriller">Thriller</option>
                   <option value="War">War</option>
                   <option value="Western">Western</option>
-                </select>
+                </select> */}
 
                 {/* <div id="hide-already-played">
                   <input
